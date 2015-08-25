6ddf84e74cdc62e5992d8fe6f3a11e0f7fd41f03
chrisSuVan <chrissuvan@gmail.com>
2015-08-25 17:34:36 +0800

    inital commit Calucaltor Simple

---
+++ objective-c/CalculatorSimple/CalculatorSimple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorSimple
+//
+//  Created by Jian Su on 2015-08-25.
+//  Copyright (c) 2015 Jian Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
