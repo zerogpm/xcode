01b24ae079ea9b04e6d3ae100f2302435168ae87
chrisSuVan <chrissuvan@gmail.com>
2015-08-30 21:02:15 -0700

    add button connection

--- swift/CalculatorSimple/CalculatorSimple/ViewController.swift
+++ swift/CalculatorSimple/CalculatorSimple/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var total:Int = 0
+    var mode:Int = 0
+    var valueString:String! = ""
+    var lastButtonWasMode:Bool = false
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func tappedNumber(sender: UIButton) {
+    }
+    
+    @IBAction func tappedPlus(sender: AnyObject) {
+    }
+    
+    @IBAction func tappedMinu(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func tappedMutple(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func tappedEqual(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func tappedClear(sender: AnyObject) {
+    }
+    
+    func MySetMode(m:Int) {
+        
     }
 
 
