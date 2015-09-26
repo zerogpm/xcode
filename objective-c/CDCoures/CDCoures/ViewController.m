9147176c4eeb5d2fd560ab9b1907f2dfd6c7d736
chrisSuVan <chrissuvan@gmail.com>
2015-09-25 17:33:13 -0700

    core data

---
+++ objective-c/CDCoures/CDCoures/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CDCoures
+//
+//  Created by Jian Su on 2015-09-25.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
