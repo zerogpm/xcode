aed57d54fe953118eab65c563382d737c558217a
chrisSuVan <chrissuvan@gmail.com>
2015-09-28 17:01:07 -0700

    add init and create layout for Tapper

---
+++ swift/Tapper/Tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper
+//
+//  Created by Jian Su on 2015-09-28.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
