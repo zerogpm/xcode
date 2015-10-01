a7c6581a9daf4e1a215b0b2f7bae0426f57e7a34
chrisSuVan <chrissuvan@gmail.com>
2015-09-30 22:37:17 -0700

    multiple project init

---
+++ swift/Multiples/Multiples/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Multiples
+//
+//  Created by Jian Su on 2015-09-29.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
