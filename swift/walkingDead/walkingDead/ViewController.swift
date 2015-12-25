ac9e6a45a53bda0059c47b4b3d24ec49dc3816af
Chris Su <zerogpm@gmail.com>
2015-12-25 12:01:02 -0800

    walking dead finished

--- swift/walkingDead/walkingDead/ViewController.swift
+++ swift/walkingDead/walkingDead/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 240
+    let HIGHT: CGFloat = 288
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i = 1; i <= 5; i++ {
+            let img = UIImage(named: "\(i)")
+            let imgView = UIImageView(image: img)
+            scrollView.addSubview(imgView)
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(i)), 425, WIDTH, HIGHT)
+        }
+        
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
