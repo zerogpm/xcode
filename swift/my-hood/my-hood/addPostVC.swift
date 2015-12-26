5611ecb8ee89ca6b8abc7a8eb77feb383914ee52
Chris Su <zerogpm@gmail.com>
2015-12-26 11:39:43 -0800

    my hood second view

---
+++ swift/my-hood/my-hood/addPostVC.swift
@@ -0,0 +1,18 @@
+//
+//  addPostVC.swift
+//  my-hood
+//
+//  Created by Jian Su on 12/26/15.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+import UIKit
+
+class addPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
