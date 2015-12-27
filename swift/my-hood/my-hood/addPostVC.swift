208fc32fefa145b316ba3ec56fa92a587332f1c3
Chris Su <zerogpm@gmail.com>
2015-12-26 21:42:01 -0800

    image picker working

--- swift/my-hood/my-hood/addPostVC.swift
+++ swift/my-hood/my-hood/addPostVC.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class addPostVC: UIViewController {
+class addPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var descriptionField: UITextField!
     
+    var imagePicker:UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         postImg.layer.cornerRadius = postImg.frame.size.width / 2
         postImg.clipsToBounds = true
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
     
     
     @IBAction func addPicBtnPressed(sender: UIButton) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostBtnPress(sender: AnyObject) {
-        
+        if let title = titleField.text , let desc = descriptionField.text, let img = postImg.image {
+            
+        }
     }
     
     @IBAction func cancelBtnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
     }
 }
