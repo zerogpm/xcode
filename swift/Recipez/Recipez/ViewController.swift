ca665aa249e31bcd13079ef756feffe441e33fb9
Chris Su <zerogpm@gmail.com>
2016-01-02 15:57:15 -0800

    Reciperz init

---
+++ swift/Recipez/Recipez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipez
+//
+//  Created by Jian Su on 12/27/15.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
