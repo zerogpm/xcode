8bd1719941d29b80a96b9db9b74a9cbbfa868d11
chrisSuVan <chrissuvan@gmail.com>
2016-02-28 17:05:19 -0800

    init app

---
+++ BigProject/swift/dispensing-fee/dispensing-fee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dispensing-fee
+//
+//  Created by Jian Su on 2016-02-28.
+//  Copyright © 2016 ZeroTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
