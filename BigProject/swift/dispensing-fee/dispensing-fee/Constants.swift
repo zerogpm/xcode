f7cd5a787038aac51b6f1b11bf44ef5b7cef1d28
Chris Su <zerogpm@gmail.com>
2016-02-28 22:20:59 -0800

    add font noto

---
+++ BigProject/swift/dispensing-fee/dispensing-fee/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  dispensing-fee
+//
+//  Created by Jian Su on 2/28/16.
+//  Copyright © 2016 ZeroTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
