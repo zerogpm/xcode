aede600bc432993b00a1cc65a5c21db16a62a098
Chris Su <zerogpm@gmail.com>
2016-06-26 22:47:29 -0700

    done with part 1

--- BigProject/swift/YouTube/YouTube/ViewController.swift
+++ BigProject/swift/YouTube/YouTube/ViewController.swift
@@ -56,6 +56,13 @@ class VideoCell: UICollectionViewCell {
     return view
   }()
   
+  let subtitleTextView: UITextView = {
+    let textView = UITextView()
+    textView.backgroundColor = UIColor.redColor()
+    textView.translatesAutoresizingMaskIntoConstraints = false
+    return textView
+  }()
+  
   let userProfileImageView:UIImageView = {
     let imageView = UIImageView()
     imageView.backgroundColor = UIColor.greenColor()
@@ -65,13 +72,23 @@ class VideoCell: UICollectionViewCell {
   let thumbnailImageView:UIImageView = {
     let imageView = UIImageView()
     imageView.backgroundColor = UIColor.blueColor()
+    imageView.image = UIImage(named: "taylor_swift_blank_space")
     return imageView
+  }()
+  
+  let titleLabel:UILabel = {
+    let label = UILabel()
+    label.backgroundColor = UIColor.purpleColor()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
   }()
   
   func setupViews() {
     addSubview(thumbnailImageView)
     addSubview(separatorView)
     addSubview(userProfileImageView)
+    addSubview(titleLabel)
+    addSubview(subtitleTextView)
     
     addConstraintsWithFormat("H:|-16-[v0]-16-|", views: thumbnailImageView)
     
@@ -83,7 +100,34 @@ class VideoCell: UICollectionViewCell {
     addConstraintsWithFormat("H:|[v0]|", views: separatorView)
     
     thumbnailImageView.frame = CGRectMake(0, 0, 100, 100)
+    
+    //top constraint
+    addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Top, relatedBy: .Equal, toItem: thumbnailImageView, attribute: .Bottom, multiplier: 1, constant: 8))
+    
+    //left constraint
+    addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Left, relatedBy: .Equal, toItem: userProfileImageView, attribute: .Right, multiplier: 1, constant: 8))
+    
+    //right constraint
+    addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Right, relatedBy: .Equal, toItem: thumbnailImageView, attribute: .Right, multiplier: 1, constant: 0))
+    
+    //height constraint
+    addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .Height, relatedBy: .Equal, toItem: self  , attribute: .Height, multiplier: 0, constant: 20))
+    
+    
+    ///
+    //top constraint
+    addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .Top, relatedBy: .Equal, toItem: titleLabel, attribute: .Bottom, multiplier: 1, constant: 8))
+    
+    //left constraint
+    addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .Left, relatedBy: .Equal, toItem: userProfileImageView, attribute: .Right, multiplier: 1, constant: 8))
+    
+    //right constraint
+    addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .Right, relatedBy: .Equal, toItem: thumbnailImageView, attribute: .Right, multiplier: 1, constant: 0))
+    
+    //height constraint
+    addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .Height, relatedBy: .Equal, toItem: self, attribute: .Height, multiplier: 0, constant: 20))
   }
+  
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
