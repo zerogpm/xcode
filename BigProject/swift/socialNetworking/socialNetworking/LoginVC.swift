50bd899ae177f3c04b52c3dc5de05ca967b5a329
Chris Su <zerogpm@gmail.com>
2016-08-30 00:57:55 -0700

    new social network

--- BigProject/swift/socialNetworking/socialNetworking/LoginVC.swift
+++ BigProject/swift/socialNetworking/socialNetworking/LoginVC.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
 
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var scroll: UIScrollView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("r")
+    textField.delegate = self
+    scroll.backgroundColor = UIColor(red: 16/255, green: 19/255, blue: 19/255, alpha: 1.0)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
   }
-
-
+  
+  func textFieldDidBeginEditing(textField: UITextField) {
+    scroll.setContentOffset(CGPointMake(0, 200), animated: true)
+  }
+  
+  func textFieldDidEndEditing(textField: UITextField) {
+    scroll.setContentOffset(CGPointMake(0, 0), animated: true)
+  }
 }
 
