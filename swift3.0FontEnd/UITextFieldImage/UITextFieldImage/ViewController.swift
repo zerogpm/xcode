6760057b2a89c7ff9938929147a3db4a8ddcbd05
chrisSuVan <chrissuvan@gmail.com>
2017-02-13 13:39:49 -0800

    image inside input text

---
+++ swift3.0FontEnd/UITextFieldImage/UITextFieldImage/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UITextFieldImage
+//
+//  Created by Jian Su on 2017-01-31.
+//  Copyright © 2017 ZeroTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var inputField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+
+}
+
