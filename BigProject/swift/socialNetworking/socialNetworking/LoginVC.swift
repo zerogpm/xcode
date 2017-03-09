a441e045678a7517ce478ab2f42952e90f4b680d
Chris Su <zerogpm@gmail.com>
2017-03-08 20:30:57 -0800

    social

--- BigProject/swift/socialNetworking/socialNetworking/LoginVC.swift
+++ BigProject/swift/socialNetworking/socialNetworking/LoginVC.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class LoginVC: UIViewController, UITextFieldDelegate {
 
-  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var email: UITextField!
+  @IBOutlet weak var password: UITextField!
+  @IBOutlet weak var firstNameTextField: UITextField!
+  @IBOutlet weak var lastNameTextField: UITextField!
   @IBOutlet weak var scroll: UIScrollView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    textField.delegate = self
+    lastNameTextField.delegate = self
+    firstNameTextField.delegate = self
     scroll.backgroundColor = UIColor(red: 16/255, green: 19/255, blue: 19/255, alpha: 1.0)
   }
   
-  func textFieldShouldReturn(textField: UITextField) -> Bool {
-    textField.resignFirstResponder()
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    lastNameTextField.resignFirstResponder()
+    firstNameTextField.resignFirstResponder()
     return true
   }
   
-  func textFieldDidBeginEditing(textField: UITextField) {
-    scroll.setContentOffset(CGPointMake(0, 100), animated: true)
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    scroll.setContentOffset(CGPoint(x: 0, y: 100), animated: true)
   }
   
-  func textFieldDidEndEditing(textField: UITextField) {
-    scroll.setContentOffset(CGPointMake(0, 0), animated: true)
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    scroll.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
   }
+  
+  @IBAction func RegisterTapped(_ sender: UIButton) {
+    if firstNameTextField.text!.isEmpty || lastNameTextField.text!.isEmpty || password.text!.isEmpty || email.text!.isEmpty {
+      firstNameTextField.attributedPlaceholder = NSAttributedString(string: "First Name", attributes: [NSForegroundColorAttributeName : UIColor.red])
+      lastNameTextField.attributedPlaceholder = NSAttributedString(string: "Last Name", attributes: [NSForegroundColorAttributeName  : UIColor.red])
+      email.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSForegroundColorAttributeName : UIColor.red])
+      password.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSForegroundColorAttributeName : UIColor.red])
+    } else {
+      
+    }
+  }
+  
 }
 
